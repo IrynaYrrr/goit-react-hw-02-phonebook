ef3fd3b7d8a3e3e6b27837496bec3990e8b74ec5
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-08-08 13:46:32 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,17 +9,34 @@ export class App extends Component {
     name: ''
   }
 
-  nameInputId = nanoid();
+  createContact = (state) => {
+    const newContact = {
+      name: state.name,
+      id: nanoid(),
+    }
+    this.setState((prev) => ({
+      ...state,
+      contacts: [newContact, ...state.contacts],
+    }))
+    console.log(state);
+  }
 
-  handleChange = evt => {
-    this.setState({ inputValue: evt.target.value });
+  handleChange = ({ target: { value } }) => {
+    this.setState({ name: value })
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e);
+    this.createContact(this.state)
   };
 
   render() {
-    const { inputValue } = this.state;
+    const { name } = this.state;
+
     return (
       <div style={{
-        height: '60vh',
+        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
@@ -28,17 +45,17 @@ export class App extends Component {
         color: '#010101'
       }}>
 
-        <form style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          border: '2px solid black',
-          padding: '30px'
-        }}>
+        <form onSubmit={this.handleSubmit}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            border: '2px solid black',
+            padding: '30px'
+          }}>
           <label htmlFor={this.nameInputId}>Name</label>
           <input
-            id={this.nameInputId}
-            value={inputValue}
+            value={this.state.name}
             onChange={this.handleChange}
             type="text"
             name="name"
@@ -46,9 +63,19 @@ export class App extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
-          <button type='submit'>Add contact</button>
+          <button style={{
+            height: '30px',
+            marginTop: '20px',
+            cursor: 'pointer'
+          }}
+            type='submit'>
+            add contact
+          </button>
         </form>
         <p>Contacts</p>
+        <ul>
+          {this.state.contacts.map(c => <li>{c.name}</li>)}
+        </ul>
       </div>
     );
   }
